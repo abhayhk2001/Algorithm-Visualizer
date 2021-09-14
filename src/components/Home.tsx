33d0419c2878356cc8589cd91fdf3df6b8e9e402
Abhay H Kashyap <abhayastro1004@gmail.com>
2021-09-15 00:00:57 +0530

    ✨ (feat) Home Page completed.
    Add the home page with background image and intergrate with
    Navbar and Footer. Remove text shadow change when hovering for the links
    in Navbar. Add background image to the resources directory.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import "./static/Home.scss"
 
 function Home() {
 	return (
-		<div>
-			Home
+		<div className="home">
+			<div className="main-heading">
+				Graph Algorithm Visualizer and Solver
+			</div>
+			<div className="description">
+				<p className="warshal-desc"><span className="para-heading">Warshall’s Algorithm</span>: Computing the transitive closure of a directed graph using a Dynamic Programming method.</p>
+
+				<p className="floyds-desc"><span className="para-heading">Floyd’s Algorithm:</span> Solves the All Pairs Shortest Path problem of a edge weighted directed Graph using Dynamic Programming.</p>
+			</div>
 		</div>
 	)
 }
